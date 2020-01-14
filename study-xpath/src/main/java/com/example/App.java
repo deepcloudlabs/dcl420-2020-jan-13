87c3b8932973513f9c9d14c5b31ab95ad43db5e1
binnur.kurt <binnur.kurt@gmail.com>
2020-01-14 11:45:30 +0300

    remove warning

--- study-xpath/src/main/java/com/example/App.java
+++ study-xpath/src/main/java/com/example/App.java
@@ -3,7 +3,6 @@ package com.example;
 import static org.joox.JOOX.$;
 
 import java.io.File;
-import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
@@ -18,21 +17,14 @@ public class App {
 		// JAXB : Object <==> XML
 		// Marshal: Object ==> XML
 		// Unmarshal: XML ==> Object
-		JAXBContext context = 
-				JAXBContext.newInstance(MovieCollection.class);
-		Unmarshaller unmarshaller = 
-				context.createUnmarshaller();
-		File file = new File("src/main/resources","movies.xml");
-		MovieCollection movieCollection =
-				(MovieCollection) unmarshaller.unmarshal(file);
+		JAXBContext context = JAXBContext.newInstance(MovieCollection.class);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		File file = new File("src/main/resources", "movies.xml");
+		MovieCollection movieCollection = (MovieCollection) unmarshaller.unmarshal(file);
 //		movieCollection.getMovies()
 //		               .forEach(System.out::println);
-		Match match = $(movieCollection).xpath(
-				"//movie[number(year) >= 1970 " +
-		        " and number(year) < 1980 " +
-				" and genres/genre = 'Drama' ]"
-		);
-		match.unmarshal(Movie.class)
-		     .forEach(System.out::println);
+		Match match = $(movieCollection)
+				.xpath("//movie[number(year) >= 1970 " + " and number(year) < 1980 " + " and genres/genre = 'Drama' ]");
+		match.unmarshal(Movie.class).forEach(System.out::println);
 	}
 }
