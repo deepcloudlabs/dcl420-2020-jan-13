92646a60a798e05099e8054d050a4217fd08393f
binnur.kurt <binnur.kurt@gmail.com>
2020-01-15 16:19:00 +0300

    initial commit

---
+++ imdb-rest-client-javase/src/com/example/imdb/client/app/ImdbApp.java
@@ -0,0 +1,40 @@
+package com.example.imdb.client.app;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class ImdbApp {
+
+	public static void main(String[] args) throws Exception {
+		HttpClient client = HttpClient.newHttpClient();
+		long start = System.currentTimeMillis();
+		IntStream.range(1,200)
+		         .forEach( id -> {
+		String resourceUri = "http://localhost:8080/imdb-rest-api/api/v1/movies/"
+		         + id ;
+		HttpRequest requestForJson = 
+				HttpRequest.newBuilder()
+				           .uri(URI.create(resourceUri))
+				           .header("Accept", "application/json")
+				           .build();
+		HttpRequest requestForXml = 
+				HttpRequest.newBuilder()
+				.uri(URI.create(resourceUri))
+				.header("Accept", "application/xml")
+				.build();
+			client.sendAsync(requestForXml,
+						HttpResponse.BodyHandlers.ofString())
+			   .thenApply(HttpResponse::body)
+			   .thenAccept(System.out::println);
+      });
+		long stop = System.currentTimeMillis();
+		System.out.println("Duration : "+(stop-start)+" ms.");
+		TimeUnit.SECONDS.sleep(10);
+	}
+
+}
